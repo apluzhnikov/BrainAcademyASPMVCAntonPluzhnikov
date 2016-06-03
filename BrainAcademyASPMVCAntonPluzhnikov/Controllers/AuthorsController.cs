85377ec54309a770c4e3183f16a44afda62229d9
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2016-06-04 02:55:01 +0300

    Almost final project

--- BrainAcademyASPMVCAntonPluzhnikov/Controllers/AuthorsController.cs
+++ BrainAcademyASPMVCAntonPluzhnikov/Controllers/AuthorsController.cs
@@ -16,73 +16,123 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
         readonly private IDataObjectsManager<Library> _libraryManager;
         readonly private IDataObjectsManager<Book> _booksManager;
 
-        private Book Book;
-        private Author Author;
+        private Book _book;
+        private Author _author;
 
-        public AuthorsController() {
+        public AuthorsController()
+        {
             _authorsManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Author>>("ef");
             _libraryManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Library>>("ef");
             _booksManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Book>>("ef");
         }
 
         // GET: Authors
-        public ActionResult Index(int bookId = 0) {
+        public ActionResult Index(int bookId = 0)
+        {
             var authors = _authorsManager.GetAll();
             ViewData["bookId"] = bookId;
             return View(authors);
         }
 
         // GET: Authors/Details/5
-        public ActionResult Details(int id) {
-            return View();
+        public ActionResult Details(int id)
+        {
+            _author = _authorsManager.GetById(id);
+            return View(_author);
         }
 
-        public ActionResult AddAuthorToBook(int id, int bookId = 0) {
+        public ActionResult AddAuthorToBook(int id, int bookId = 0)
+        {
             if (bookId != 0)
-                Book = _booksManager.GetById(bookId);
-            if (id != 0)
-                Author = _authorsManager.GetById(id);
-            if (Book != null && Author != null)
             {
-                _libraryManager.Add(new Library() { BookId = Book.Id, AuthorId = Author.Id });
-                _libraryManager.SaveChanges();
+                _book = _booksManager.GetById(bookId);
+                ViewData["bookId"] = bookId;
             }
-            //return RedirectToAction("Index", "Books");
-            return RedirectToAction("Index");
+            if (id != 0)
+                _author = _authorsManager.GetById(id);
+            if (_book != null && _author != null)
+            {
+                var library = _libraryManager.GetAll();
+                if (!library.Any(arg => arg.AuthorId == _author.Id && arg.BookId == _book.Id))
+                {
+                    _libraryManager.Add(new Library() { BookId = _book.Id, AuthorId = _author.Id });
+                    _libraryManager.SaveChanges();
+                }
+            }
+            //return RedirectToAction("Index", new { bookId = bookId });
+            return RedirectToAction("Edit", "Books", new { id = bookId });
         }
 
 
         // GET: Authors/Create
-        public ActionResult Create(int bookId = 0) {
-            //currentBookId = bookId;
+        public ActionResult Create(int bookId = 0)
+        {
             return View();
         }
 
         // POST: Authors/Create
         [HttpPost]
-        public ActionResult Create(Author author) {
+        public ActionResult Create(Author author)
+        {
             try
             {
                 // TODO: Add insert logic here
                 if (!ModelState.IsValid)
                     return View();
 
-                /*if (currentBookId > 0)
-                {
-                    var book = _booksManager.GetAll().First(arg => arg.Id == currentBookId);
-                    if (book != null)
-                        _libraryManager.Add(new Library { Author = author, Book = book });
-                    else
-                        _authorsManager.Add(author);
-                } else*/
-                {
-                    _authorsManager.Add(author);
-                }
+                _authorsManager.Add(author);
                 _authorsManager.SaveChanges();
                 return RedirectToAction("Index");
 
             }
             catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var author = _authorsManager.GetById(id);
+            return View(author);
+        }
+
+        // POST: Authors/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Author author)
+        {
+            try
+            {
+                var a = _authorsManager.GetById(id);
+                a.FirstName = author.FirstName;
+                a.LastName = author.LastName;
+                _authorsManager.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var author = _authorsManager.GetById(id);
+            return View(author);
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Author author)
+        {
+            try
+            {
+                var a = _authorsManager.GetById(id);
+                _authorsManager.Remove(a);
+                _authorsManager.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
             {
                 return View();
             }
--- BrainAcademyASPMVCAntonPluzhnikov/Controllers/BooksController.cs
+++ BrainAcademyASPMVCAntonPluzhnikov/Controllers/BooksController.cs
@@ -14,31 +14,40 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
 {
     public class BooksController : Controller
     {
+        readonly private IDataObjectsManager<Author> _authorsManager;
+        readonly private IDataObjectsManager<Library> _libraryManager;
         readonly private IDataObjectsManager<Book> _booksManager;
 
-        public BooksController() {
+        public BooksController()
+        {
+            _authorsManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Author>>("ef");
+            _libraryManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Library>>("ef");
             _booksManager = UnityConfig.GetConfiguredContainer().Resolve<IDataObjectsManager<Book>>("ef");
         }
 
         // GET: Books
         [OutputCache(Duration = 120, Location = System.Web.UI.OutputCacheLocation.Server)]
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
             var books = _booksManager.GetAll();
             return View(books);
         }
 
-        public ActionResult AddAuthor(int id) {
+        public ActionResult AddAuthor(int id)
+        {
             return RedirectToAction("Index", "Authors", new { bookId = id });
         }
 
-        public ActionResult IndexRemoveCache() {
+        public ActionResult IndexRemoveCache()
+        {
             var path = Url.Action("Index", "Books");
             Response.RemoveOutputCacheItem(path);
             return RedirectToAction("Index");
         }
 
         // GET: Books/Details/5
-        public ActionResult Details(int id) {
+        public ActionResult Details(int id)
+        {
             var book = _booksManager.GetById(id);
             var hit = book.Hits.Find(arg => arg.Date == DateTime.UtcNow.Date);
             var random = new Random();
@@ -59,13 +68,11 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
 
             _booksManager.SaveChanges();
 
-
-            //hits = book.Hits;
-
             return View(book);
         }
 
-        public string GetHitsStatistics(int id) {
+        public string GetHitsStatistics(int id)
+        {
             List<Hit> hits = _booksManager.GetById(id).Hits;
             var statistic = string.Empty;
 
@@ -92,25 +99,36 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
         }
 
         // GET: Books/Create
-        public ActionResult Create() {
+        public ActionResult Create()
+        {
             return View();
         }
 
         // POST: Books/Create
         [HttpPost]
-        public ActionResult Create(Book book) {
-            //public ActionResult Create(FormCollection collection){
-
-
+        public ActionResult Create(Book book)
+        {
             try
             {
                 // TODO: Add insert logic here
                 if (!ModelState.IsValid)
                     return View();
 
+                book.Hits = new List<Hit>();
+                var random = new Random();
+                for (int i = 0; i < 8; i++)
+                {
+                    var hit = new Hit()
+                    {
+                        Date = DateTime.UtcNow.Date.AddDays(i * (-1))
+                    };
+                    hit.Count = random.Next(0, 100);
+                    book.Hits.Add(hit);
+                }
+
+
                 _booksManager.Add(book);
                 _booksManager.SaveChanges();
-                /*return RedirectToAction("Create", "Authors", new { bookId = book.Id });*/
                 return RedirectToAction("Index");
 
             }
@@ -120,23 +138,24 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
             }
         }
 
-        // GET: Persons/Edit/5
+        // GET: Books/Edit/5
         public ActionResult Edit(int id)
         {
-            var book = _booksManager.GetAll().First(x => x.Id == id);
+            var book = _booksManager.GetById(id);
             return View(book);
         }
 
-        // POST: Persons/Edit/5
+        // POST: Books/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, Book book)
         {
             try
             {
                 // TODO: Add update logic here
-                var b = _booksManager.GetAll().First(x => x.Id == id);
+                var b = _booksManager.GetById(id);
                 b.ISBN = book.ISBN;
                 b.Title = book.Title;
+                _booksManager.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -145,30 +164,32 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
             }
         }
 
-        // GET: Persons/Delete/5
+        // GET: Books/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var book = _booksManager.GetById(id);
+            return View(book);
         }
 
-        // POST: Persons/Delete/5
+        // POST: Books/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, Book book)
         {
             try
             {
-                // TODO: Add delete logic here
-                _booksManager.Remove(book);
-                //Persons.Remove(person);
+                var b = _booksManager.GetById(id);
+                _booksManager.Remove(b);
+                _booksManager.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
                 return View();
             }
         }
 
-        public string GetAuthors(Book book) {
+        public string GetAuthors(Book book)
+        {
             var b = _booksManager.GetById(book.Id);
             List<Author> authors = new List<Author>();
             if (book.Library != null)
@@ -177,5 +198,24 @@ namespace BrainAcademyASPMVCAntonPluzhnikov.Controllers
             }
             return string.Join(", ", authors);
         }
+        public ActionResult UnlinkAuthor(int id, int authorId = 0)
+        {
+            if (authorId > 0)
+            {
+                var libraryItem = _libraryManager.GetAll().FirstOrDefault(arg => arg.BookId == id && arg.AuthorId == authorId);
+                if (libraryItem != null && libraryItem.Id > 0)
+                {
+                    _libraryManager.Remove(libraryItem);
+                    _libraryManager.SaveChanges();
+                    return RedirectToAction("Edit", new { id = id });
+
+                }
+                else
+                    return View();
+            }
+            else
+                return View();
+        }
+
     }
 }
